a7c6f8e26914d90c14c419557496c130a39a7121
autopp <autopp.inc@gmail.com>
2019-12-25 00:42:23 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/autopp/hello_prometheus_exporter_go
+
+go 1.13
+
+require github.com/prometheus/client_golang v1.3.0
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	namespace = "SampleMetric"
+)
+
+type myCollector struct{}
+
+var (
+	exampleCount = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "example_count",
+		Help:      "example counter help",
+	})
+	exampleGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Name:      "example_gauge",
+		Help:      "example gauge help",
+	})
+)
+
+func (c myCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- exampleCount.Desc()
+	ch <- exampleGauge.Desc()
+}
+
+func (c myCollector) Collect(ch chan<- prometheus.Metric) {
+	exampleValue := float64(12345)
+
+	ch <- prometheus.MustNewConstMetric(
+		exampleCount.Desc(),
+		prometheus.CounterValue,
+		float64(exampleValue),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		exampleGauge.Desc(),
+		prometheus.GaugeValue,
+		float64(exampleValue),
+	)
+}
+
+func main() {
+	flag.Parse()
+
+	var c myCollector
+	prometheus.MustRegister(c)
+
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe("127.0.0.1:5000", nil))
+}
