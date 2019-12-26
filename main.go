0498230758777cbacb802acb0377b66737191b09
autopp <autopp.inc@gmail.com>
2019-12-26 09:42:58 +0900

    set counter value

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ const (
 	namespace = "SampleMetric"
 )
 
-type myCollector struct{}
+type myCollector struct {
+	counter int
+}
 
 var (
 	exampleCount = prometheus.NewCounter(prometheus.CounterOpts{
@@ -28,31 +30,32 @@ var (
 	})
 )
 
-func (c myCollector) Describe(ch chan<- *prometheus.Desc) {
+func (c *myCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- exampleCount.Desc()
 	ch <- exampleGauge.Desc()
 }
 
-func (c myCollector) Collect(ch chan<- prometheus.Metric) {
+func (c *myCollector) Collect(ch chan<- prometheus.Metric) {
 	exampleValue := float64(12345)
 
 	ch <- prometheus.MustNewConstMetric(
 		exampleCount.Desc(),
 		prometheus.CounterValue,
-		float64(exampleValue),
+		float64(c.counter),
 	)
 	ch <- prometheus.MustNewConstMetric(
 		exampleGauge.Desc(),
 		prometheus.GaugeValue,
 		float64(exampleValue),
 	)
+	c.counter++
 }
 
 func main() {
 	flag.Parse()
 
 	var c myCollector
-	prometheus.MustRegister(c)
+	prometheus.MustRegister(&c)
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe("127.0.0.1:5000", nil))
